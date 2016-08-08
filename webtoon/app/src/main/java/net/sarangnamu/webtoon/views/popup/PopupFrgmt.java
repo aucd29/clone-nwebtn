e9ff62a71828159cdaf3703c93d7c0ea9aea69f4
Burke Choi <aucd29@choecheoldongui-MacBook-Pro.local>
2016-08-08 15:55:42 +0900

    - popup layout 추가

---
+++ webtoon/app/src/main/java/net/sarangnamu/webtoon/views/popup/PopupFrgmt.java
@@ -0,0 +1,10 @@
+package net.sarangnamu.webtoon.views.popup;
+
+import net.sarangnamu.webtoon.views.SlideFrgmtBase;
+
+/**
+ * Created by <a href="mailto:aucd29@gmail.com">Burke Choi</a> on 2016. 8. 8.. <p/>
+ */
+public class PopupFrgmt extends SlideFrgmtBase {
+    private static final org.slf4j.Logger mLog = org.slf4j.LoggerFactory.getLogger(PopupFrgmt.class);
+}
